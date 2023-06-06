a40810d685a245f65990457d300061d23e4265da
MaksMusic <Maksmusic05@yandex.ru>
2023-06-06 15:32:26 +0300

    edit

---
+++ src/Class1.java
@@ -0,0 +1,9 @@
+public class Class1 {
+    public static void main(String[] args) {
+        int a  = 100;
+        int b  = 200;
+
+        System.out.println(a + b);
+        System.out.println();
+    }
+}
